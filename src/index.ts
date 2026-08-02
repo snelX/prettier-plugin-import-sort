f52f6a927720ed0a6581a651c64aca0342b05410
Herberth Obregón <herberthobregon@gmail.com>
2026-08-02 02:19:53 -0600

    chore: Bump version to 1.1.1 and simplify directory resolution in import organization

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import path from "path";
-import { fileURLToPath } from "url";
 
 import sortImports from "import-sort";
 import { getConfig } from "import-sort-config";
@@ -7,8 +6,6 @@ import { parsers as javascriptParsers } from "prettier/parser-babel";
 import { parsers as typescriptParsers } from "prettier/parser-typescript";
 
 import * as typescriptParser from "./parsers/typescript.js";
-
-const moduleDirname = path.dirname(fileURLToPath(import.meta.url));
 
 function getAndCheckConfig(extension: string, fileDirectory?: string) {
 	const resolvedConfig = getConfig(extension, fileDirectory);
@@ -27,7 +24,7 @@ function getAndCheckConfig(extension: string, fileDirectory?: string) {
 }
 
 function organizeImports(unsortedCode: string, extension: string, dirname: string | null, filepath: string | undefined): string {
-	const config = getAndCheckConfig(extension, dirname || path.resolve(moduleDirname, "..", ".."));
+	const config = getAndCheckConfig(extension, dirname || process.cwd());
 	const { parser, style, config: rawConfig } = config;
 
 	const usesTypescriptParser = rawConfig.parser === "typescript" || /import-sort-parser-typescript/.test(String(parser));
